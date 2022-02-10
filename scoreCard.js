415eee319b63f0f268277f02c7fb7c6e6a8d12f6
Swati002 <swatigoel37@gmail.com>
2022-02-10 21:25:47 +0530

    first commit

--- scoreCard.js
+++ scoreCard.js
@@ -1,32 +1,90 @@
-const url = 'https://www.espncricinfo.com//series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard'
+// const url =
+//   "https://www.espncricinfo.com//series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
 
+const request = require("request");
+const cheerio = require("cheerio");
 
-const request = require('request');
-const cheerio = require('cheerio');
-
-request(url, cb)
+function processScoreCrad(url) {
+    request(url, cb);
+}
 
 function cb(err, response, html) {
     if (err) {
-        console.log(err)
+        console.error(err);
     } else {
-        // console.log(html)
-        extractMatchDetails(html)
+        extractMatchDetails(html);
     }
 }
 
 function extractMatchDetails(html) {
-    let $ = cheerio.load(html)
+    let $ = cheerio.load(html);
 
-    let descString = $('.header-info .description')
-    let descStringArr = descString.text().split(',')
+    let descString = $(".match-header-info.match-info-MATCH .description");
 
+    let descStringArr = descString.text().split(",");
 
     let venue = descStringArr[1].trim();
     let date = descStringArr[2].trim();
-    let result = $('.match-info.match-info-MATCH.match-info-MATCH-half-width .status-text span').text()
+
+    let result = $(
+        ".match-info.match-info-MATCH.match-info-MATCH-half-width .status-text span"
+    ).text();
+
     console.log(venue);
     console.log(date);
     console.log(result);
 
+    console.log("```````````````````````````````````````````````````");
+
+    let innings = $(".card.content-block.match-scorecard-table>.Collapsible");
+
+    //console.log(innings)
+
+    let htmlString = "";
+
+    for (let i = 0; i < innings.length; i++) {
+        htmlString += $(innings[i]).html();
+
+        let teamName = $(innings[i]).find("h5").text();
+        teamName = teamName.split("INNINGS")[0].trim();
+        let opponentIndex = i == 0 ? 1 : 0;
+
+        let opponentName = $(innings[opponentIndex]).find("h5").text();
+        opponentName = opponentName.split("INNINGS")[0].trim();
+
+        //console.log(teamName , opponentName);
+
+        let cInning = $(innings[i]);
+
+        let allRows = cInning.find(".table.batsman tbody tr");
+
+        for (let j = 0; j < allRows.length; j++) {
+            let allCols = $(allRows[j]).find("td");
+            let isWorthy = $(allCols[0]).hasClass("batsman-cell");
+
+            if (isWorthy == true) {
+                let playerName = $(allCols[0]).text().trim();
+
+                let runs = $(allCols[2]).text().trim();
+                let balls = $(allCols[3]).text().trim();
+                let fours = $(allCols[5]).text().trim();
+                let sixes = $(allCols[6]).text().trim();
+                let STR = $(allCols[7]).text().trim();
+
+                console.log(
+                    `${playerName} | ${runs} |${balls} | ${fours} | ${sixes} | ${STR}`
+                );
+                // Template Literal
+            }
+        }
+
+        console.log("````````````````````````````````````````````````````````");
+    }
+
+    //console.log(htmlString)
+}
+
+
+module.exports = {
+    ps: processScoreCrad
 }
